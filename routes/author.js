210e9935f2af7bda77e8d229ef15306472c1a539
dizzysoup <j481936@gmail.com>
2023-12-10 20:06:53 +0800

    backup01

---
+++ routes/author.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var db = require('../db');
+var router = express.Router();
+
+router.get('/', async function(req, res, next) {
+    const con = await db;
+    const rows = await con.query('SELECT * from  Author ;');
+    console.table(rows);
+
+    res.render('author');
+  });
+  
+  module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,32 +1,8 @@
 var express = require('express');
-var db = require('../db');
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  db.getConnection()
-  .then(conn => {
-  
-    conn.query("SELECT 1 as val")
-      .then((rows) => {
-        console.log(rows); //[ {val: 1}, meta: ... ]
-        //Table must have been created before 
-        // " CREATE TABLE myTable (id int, val varchar(255)) "
-        
-      })
-      .then((res) => {
-        console.log(res); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
-        conn.end();
-      })
-      .catch(err => {
-        //handle error
-        console.log(err); 
-        conn.end();
-      })
-      
-  }).catch(err => {
-    //not connected
-  });
   res.send('respond with a resource');
 });
 
